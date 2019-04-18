0196487b602ea8e803e39903f121c1782aa7485b
unknown <guoqiang.fu@hirain.com>
2019-04-18 18:53:01 +0800

    格式化工具和编辑器配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-alert': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //强制使用单引号
+    quotes: ['error', 'single'],
+    //强制不使用分号结尾
+    semi: ['error', 'never']
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
-};
+}
